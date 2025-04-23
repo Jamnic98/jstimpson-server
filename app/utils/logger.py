3b51c8ab7f64c41d8cd8b73260e495602046510a
Jamie Stimpson <Jamnic98>
2025-04-23 15:14:10 +0100

    fix logging for lambda function

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,19 +1,12 @@
 import logging
 
 
-def get_logger():
-    log = logging.getLogger(__name__)
+logger = logging.getLogger()
 
-    if not log.hasHandlers():  # Prevent duplicate handlers
-        log.setLevel(logging.DEBUG)
-        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
 
-        log.addHandler(stream_handler)
-        log.propagate = False  # Prevent duplicate logs
-
-    return log
-
-logger = get_logger()
+logger.addHandler(stream_handler)
