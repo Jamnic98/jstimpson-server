74e18e4e13e17589c4037f63b1e5209eda8e8b49
Jamie Stimpson <Jamnic98>
2025-04-24 22:11:43 +0100

    update and refactor

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,14 +3,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_URI: str = "mongodb://localhost:27017"
     DB_NAME: str = "dev_database"
+    DB_URI: str = "mongodb://localhost:27017"
+    DEBUG: bool = True
+    PORT: int = 8080
+    RELOAD: bool = True
     STRAVA_CLIENT_ID: Optional[str] = None
     STRAVA_CLIENT_SECRET: Optional[str] = None
-    DEBUG: bool = True
-    RELOAD: bool = True
-    PORT: int = 8080
 
-    model_config = SettingsConfigDict(case_sensitive=True)
+    model_config = SettingsConfigDict(env_file='.env.local', case_sensitive=True)
 
 settings: Settings = Settings()
